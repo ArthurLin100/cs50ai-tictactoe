25b8654127d7bb0aadfd50682f6e10229ff2bebd
ArthurLin100 <thur.lin@gmail.com>
2026-01-31 18:56:03 +0900

    implemented winner function'

--- tictactoe.py
+++ tictactoe.py
@@ -64,7 +64,23 @@ def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    # Check rows
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2] != EMPTY:
+            return board[i][0]
+        
+    # Check columns
+    for j in range(3):
+        if board[0][j] == board[1][j] == board[2][j] != EMPTY:
+            return board[0][j]
+    
+    # Check diagonals
+    if board[0][0] == board[1][1] == board[2][2] != EMPTY:
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] != EMPTY:
+        return board[0][2]
+
+    return None    
 
 
 def terminal(board):
